f6cf4e3d7f95d62c25c6714061a61df2cd8f7522
alicansadeler <alicansadeler@gmail.com>
2024-07-23 16:26:00 +0300

    tamamlandi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,54 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello world!2");
+        boolean result = isPalindrome(-1221);
+        System.out.println(result);
+  }
+    public static boolean isPalindrome(int number) {
+       int newNumber = Math.abs(number);
+        String numberString = String.valueOf(newNumber);
+        String palindromeNumber = new StringBuilder(numberString).reverse().toString();
+
+        if (numberString.equals(palindromeNumber)) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+
+    public static boolean isPerfectNumber(int number) {
+        int result = 0;
+
+        for (int i=1; i < number; i++) {
+            if (number % i == 0) {
+                result += i;
+            }
+        }
+        return  result == number;
+    };
+
+    public static String numberToWords(int number) {
+
+        if (number < 0) {
+            return "Invalid Value";
+        }
+
+        String[] words = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+
+        ArrayList<String> result = new ArrayList<>();
+
+        while (number > 0) {
+            int sonRakam = number % 10;
+            result.add(0,words[sonRakam]);
+            number = number / 10;
+        }
+
+        return String.join(" ", result);
     }
 }
